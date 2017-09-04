4077e071254cbad490874c3b3615e04123a7b413
German Gonzalez <german.gonzalez@gmail.com>
2017-09-04 12:30:02 -0300

    checking jdbc version

--- src/cl/devweb/sample/jndi/JndiTreeRecursive.java
+++ src/cl/devweb/sample/jndi/JndiTreeRecursive.java
@@ -15,52 +15,52 @@ import org.apache.commons.collections4.MapUtils;
 public class JndiTreeRecursive {
 
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-		System.out.println("start.");
+        System.out.println("start.");
 
-		Properties env = new Properties();
-		env.put(Context.INITIAL_CONTEXT_FACTORY, "weblogic.jndi.WLInitialContextFactory");
-		env.put(Context.SECURITY_PRINCIPAL, "weblogic");
-		env.put(Context.SECURITY_CREDENTIALS, "welcome1");
-		env.put(Context.PROVIDER_URL, "t3://127.0.0.1:7001/");
-		Context context = new InitialContext(env);
+        Properties env = new Properties();
+        env.put(Context.INITIAL_CONTEXT_FACTORY, "weblogic.jndi.WLInitialContextFactory");
+        env.put(Context.SECURITY_PRINCIPAL, "weblogic");
+        env.put(Context.SECURITY_CREDENTIALS, "welcome1");
+        env.put(Context.PROVIDER_URL, " t3://127.0.0.1:7001/");
+        Context context = new InitialContext(env);
 
-		Map map = JndiTreeRecursive.toMap(context);
-		//System.out.println(map);
+        Map map = JndiTreeRecursive.toMap(context);
+        //System.out.println(map);
 
-		MapUtils.debugPrint(System.out, "myMap", map);
-		System.out.println("end.");
-	}
+        MapUtils.debugPrint(System.out, "myMap", map);
+        System.out.println("end.");
+    }
 
 
 
-	public static Map toMap(Context ctx) throws NamingException {
+    public static Map toMap(Context ctx) throws NamingException {
 
-	    String namespace = ctx instanceof InitialContext ? ctx.getNameInNamespace() : "";
-	    HashMap<String, Object> map = new HashMap<String, Object>();
-	    System.out.println("> Listing namespace: " + namespace);
-	    NamingEnumeration<NameClassPair> list = ctx.list(namespace);
-	    while (list.hasMoreElements()) {
-	        NameClassPair next = list.next();
-	        String name = next.getName();
-	        String jndiPath = namespace + name;
-	        Object lookup;
-	        try {
-	        	System.out.println("> Looking up name: " + jndiPath);
-	            Object tmp = ctx.lookup(jndiPath);
-	            if (tmp instanceof Context) {
-	                lookup = toMap((Context) tmp);
-	            } else {
-	                lookup = tmp.toString();
-	            }
-	        } catch (Throwable t) {
-	            lookup = t.getMessage();
-	        }
-	        map.put(name, lookup);
+        String namespace = ctx instanceof InitialContext ? ctx.getNameInNamespace() : "";
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        System.out.println("> Listing namespace: " + namespace);
+        NamingEnumeration<NameClassPair> list = ctx.list(namespace);
+        while (list.hasMoreElements()) {
+            NameClassPair next = list.next();
+            String name = next.getName();
+            String jndiPath = namespace + name;
+            Object lookup;
+            try {
+                System.out.println("> Looking up name: " + jndiPath);
+                Object tmp = ctx.lookup(jndiPath);
+                if (tmp instanceof Context) {
+                    lookup = toMap((Context) tmp);
+                } else {
+                    lookup = tmp.toString();
+                }
+            } catch (Throwable t) {
+                lookup = t.getMessage();
+            }
+            map.put(name, lookup);
 
-	    }
-	    return map;
-	}
+        }
+        return map;
+    }
 
 }
---
+++ src/cl/devweb/sample/jndi/TestJdbc.java
@@ -0,0 +1,68 @@
+package cl.devweb.sample.jndi;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestJdbc
+ */
+public class TestJdbc extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestJdbc() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.getWriter().append("Served at: ").append(request.getContextPath());
+
+        // from https://docs.oracle.com/cd/B28359_01/java.111/b31224/getsta.htm
+        Context ctx;
+        try {
+            ctx = new InitialContext();
+
+            javax.sql.DataSource ds = (javax.sql.DataSource) ctx.lookup("JDBC/VISTA_360_CN");
+            Connection conn = ds.getConnection();
+
+            // Create Oracle DatabaseMetaData object
+            DatabaseMetaData meta = conn.getMetaData();
+
+            // gets driver info:
+            System.out.println("JDBC driver version is " + meta.getDriverVersion());
+            response.getWriter().append("\nJDBC driver version is ").append(meta.getDriverVersion());
+
+        } catch (NamingException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        doGet(request, response);
+    }
+
+}
